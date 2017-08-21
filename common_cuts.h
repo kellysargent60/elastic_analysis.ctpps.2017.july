eb4702c15ee2fa569b779f5e9881c599c77b1fcf
Jan Kašpar <jan.kaspar@cern.ch>
2017-08-21 13:01:52 +0200

    Enabled cuts: 5, 6, 1 and 2.

--- common_cuts.h
+++ common_cuts.h
@@ -24,14 +24,14 @@ void Analysis::BuildCuts()
 	ccb[3] = 1.;
 	ccc[3] = cut3_c;
 	csi[3] = cut3_si;
-	cuts.push_back(3);
+	//cuts.push_back(3);
 	
 	cqaN[4] = "#theta_{x}^{L}"; cqbN[4] = "vtx_{x}^{L}";
 	cca[4] = -cut4_a;
 	ccb[4] = 1.;
 	ccc[4] = cut4_c;
 	csi[4] = cut4_si;
-	cuts.push_back(4);
+	//cuts.push_back(4);
 	
 	cqaN[5] = "y^{R,2,F}"; cqbN[5] = "y^{R,2,F} - y^{R,1,F}";
 	cca[5] = -cut5_a;
@@ -45,7 +45,7 @@ void Analysis::BuildCuts()
 	ccb[6] = 1.;
 	ccc[6] = cut6_c;
 	csi[6] = cut6_si;
-	//cuts.push_back(6);
+	cuts.push_back(6);
 	
 	cqaN[7] = "#theta_{x}"; cqbN[7] = "vtx_{x}^{R} - vtx_{x}^{L}";
 	cca[7] = -cut7_a;
